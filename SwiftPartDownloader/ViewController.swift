a916f95310f2be04b9a80bb57cb0c0da3266b80c
Jordan Lu <JordanLu@Jordans-MacBook-Pro.local>
2014-07-17 10:24:03 -0400

    Initial Commit

---
+++ SwiftPartDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPartDownloader
+//
+//  Created by Jordan Lu on 7/17/14.
+//  Copyright (c) 2014 jordanLu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
